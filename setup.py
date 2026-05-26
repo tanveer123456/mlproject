8fbc9f2c539032d85af2883ef2626bbdfc71b3c9
tanveer123456 <tanveerchougule1@gmail.com>
2026-05-26 11:33:15 +0530

    Project setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+HYPEN_E_DOT='_e.'
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> list[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='MLProject',
+    version='0.0.1',
+    author='Tanveer',
+    author_email='tanveerchougule1@gmail.com',
+    packages=find_packages(),
+    install_requires=getRequirements()
+)
