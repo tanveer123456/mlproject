bf9ff1cefd0e3ad58e64904a29e0f793f58ce7d5
tanveer123456 <tanveerchougule1@gmail.com>
2026-05-28 15:03:15 +0530

    Data Transformation  Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     raw_data_path:str=os.path.join("artifacts",'data.csv')
@@ -32,4 +33,6 @@ class DataIngestion:
             raise CustomException(e,sys)
 if __name__ =="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
